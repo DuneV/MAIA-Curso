fc2c30bf015821513176e8c9d3843339cc8339a6
Daniel Alejandro Alvarez Vanegas <96156811+DuneV@users.noreply.github.com>
2024-01-17 04:55:11 -0500

    update

--- Esp-idf/imu_test/imu_test/main/main.cpp
+++ Esp-idf/imu_test/imu_test/main/main.cpp
@@ -1,11 +1,60 @@
 #include <stdio.h>
 #include <mpu6050.hpp>
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+// Globals, used for compatibility with Arduino-style sketches.
+namespace {
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+int inference_count = 0;
+
+constexpr int kTensorArenaSize = 2000;
+uint8_t tensor_arena[kTensorArenaSize];
 
 extern "C" {
     void app_main(void);
 }
 
 void app_main(void) {
+
+        // Map the model into a usable data structure. This doesn't involve any
+    // copying or parsing, it's a very lightweight operation.
+    model = tflite::GetModel(g_model);
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+        MicroPrintf("Model provided is schema version %d not equal to supported "
+                    "version %d.", model->version(), TFLITE_SCHEMA_VERSION);
+        return;
+    }
+
+    // Pull in only the operation implementations we need.
+    static tflite::MicroMutableOpResolver<1> resolver;
+    if (resolver.AddFullyConnected() != kTfLiteOk) {
+        return;
+    }
+
+    // Build an interpreter to run the model with.
+    static tflite::MicroInterpreter static_interpreter(
+        model, resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
+
+    // Allocate memory from the tensor_arena for the model's tensors.
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+        MicroPrintf("AllocateTensors() failed");
+        return;
+    }
+
+    // Obtain pointers to the model's input and output tensors.
+    input = interpreter->input(0);
+    output = interpreter->output(0);
+
+    // Keep track of how many inferences we have performed.
+    inference_count = 0;
 
     // clk gpio pin 23
     // sda gpio pin 22
